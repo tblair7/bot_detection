5d441d84c03f66fb5aaca5c60fd6b5aac2bc2d84
tblair7 <tyler7blair@gmail.com>
2019-05-29 19:24:15 -0700

    refactored scraping functions

--- scrape_hs.py
+++ scrape_hs.py
@@ -123,6 +123,11 @@ def scrape_names(min_rank, max_rank, skill):
 
     for i in range(min_page, max_page):
 
+        if not i%10:
+            print('Pages scraped: {0}/{1}'.format(i-min_page, pages), datetime.utcnow())
+            conn.commit()
+
+
         url = total_base + str(i)
         soup = BeautifulSoup(requests.get(url).text, 'html.parser')
         try:
@@ -148,20 +153,124 @@ def scrape_names(min_rank, max_rank, skill):
                         print('Failed:', cols)
                         continue
 
-                else:
-                    continue
         else:
             failures.append(soup)
             continue
 
-        if not i%10:
-            print('Pages scraped: {0}/{1}'.format(i-min_page, pages), datetime.utcnow())
-            conn.commit()
-        else:
-            continue
-
     return failures
 
+#### scrape all player names
+
+def get_all_players():
+    import requests, pyspark
+    import sqlite3 as sql
+    import pandas as pd
+    import pickle as pckl
+    from bs4 import BeautifulSoup
+    from pyspark.sql.types import DateType
+    from pyspark.sql import SparkSession
+    from datetime import datetime
+
+    conn = sql.connect('./data/osrs.db')
+
+    spark = SparkSession.builder.appName("osrs").getOrCreate()
+
+    sqlContext = pyspark.sql.SQLContext(spark)
+
+    now = datetime.utcnow()
+
+    players_results = conn.execute('SELECT name, date FROM players_name LIMIT 50').fetchall()
+    players_df = pd.DataFrame({'name': [x[0] for x in players_results], 'date': [x[1] for x in players_results]})
+    players = sqlContext.createDataFrame(players_df).select('name').repartition(16)
+
+    players_hs = players.rdd.mapPartitions(get_single_player_part)
+    players_hs.is_checkpointed = True
+
+    print('Checkpoint enabled: ', players_hs.is_checkpointed,'\nNum partitions: ', players_hs.getNumPartitions())
+    print(now)
+
+    results = players_hs.collect()
+
+    # insert dataframe function
+    try:
+        xp_df, rank_df = data_to_df(results)
+    except:
+        return results
+
+    print(datetime.utcnow())
+
+    return xp_df, rank_df
+
+
+# player scraping results into dataframe
+
+def data_to_df(data):
+
+    columns = ['name', 'date', 'overall', 'attack', 'defence', 'strength', 'hitpoints', 'ranged', 'prayer', 'magic',
+               'cooking', 'woodcutting', 'fletching', 'fishing', 'firemaking', 'crafting', 'smithing', 'mining',
+               'herblore', 'agility', 'thieving', 'slayer', 'farming', 'runecraft', 'hunter', 'construction']
+
+    xp_list, rank_list = [], []
+
+    for row in data:
+        xp = [row[0]] + [row[1]] + [int(x) for x in row[2]]
+        rank = [row[0]] + [row[1]] + [int(x) for x in row[3]]
+
+        xp_zip = zip(columns, xp)
+        rank_zip = zip(columns, rank)
+
+        xp_dict, rank_dict = {}, {}
+
+        for key, value in xp_zip:
+            xp_dict[key] = value
+
+        for key, value in rank_zip:
+            rank_dict[key] = value
+
+        xp_list.append(xp_dict)
+        rank_list.append(rank_dict)
+
+    xp_df = xp_df.append(xp_list)
+    rank_df = rank_df.append(rank_list)
+
+    return xp_df, rank_df
+
+
+
+
+def get_single_player_part(players):
+    from datetime import datetime
+    # base url for api request
+    player_url_base = 'https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player='
+
+    i = 0
+    now = datetime.utcnow()
+
+    my_list = []
+
+    for player in players:
+        name = player['name']
+        url = player_url_base + name
+
+        if not i%20:
+            now = datetime.utcnow()
+        else:
+            pass
+        i += 1
+
+        try:
+            page = requests.get(url).text.replace(u'\n', u' ')
+            skills = [i.split(',') for i in page.split()]
+            xp = [i[2] for i in skills[0:24]]
+            rank = [i[0] for i in skills[0:24]]
+            my_list.append(tuple((name, now, xp, rank)))# players_data
+        except:
+            pass
+    return my_list
+
+
+
+##### if wanting to scrape for particular skill
 
 def get_player(conn, skill):
 
